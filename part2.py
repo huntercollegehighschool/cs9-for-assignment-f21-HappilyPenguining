1bd6c9371ebf7677f33d19089857306e3a9a752b
Grisha London <93930088+HappilyPenguining@users.noreply.github.com>
2021-12-20 09:53:49 +0000

    I added a number variable and a total variable, made a for loop and printed the result at the end

--- part2.py
+++ part2.py
@@ -8,3 +8,13 @@ For example, if the user types in 4, the program should print 100 (since 1^3 + 2
 """
 
 #write your code here
+
+number = int(input("What number of cubes are you looking for? "))
+
+total=0
+
+for i in range(1, number+1):
+  i = i**3
+  total= total + i
+
+print (total)
